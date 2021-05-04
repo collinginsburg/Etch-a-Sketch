7d7cf61beaca9a2ec44bc24a9b28be4944a5d8fc
collinginsburg <chginsburg@email.wm.edu>
2021-05-04 14:13:07 -0700

    after some fiddling the basic etch a sketch is now in place. my main issue was that i forgot to remove the old child nodes when resetting the game

--- script.js
+++ script.js
@@ -1,25 +1,96 @@
-let i = 1;
-let row = 100;
-let column = 100;
-let grid = row * column;
-
-const gridcontainer = document.querySelector('.gridcontainer');
-
-while (i <= grid){
-    let square = document.createElement('div');
-    square.style.cssText = "width:100%; height:100%";
-    square.classList.add(`square`, `${i}`, `unsketched`);
-    gridcontainer.appendChild(square);
-    i++;
+// remove old squares
+function removeOldSquares(){
+    squares.forEach(item => gridcontainer.removeChild(item));
 }
 
-const squares = document.querySelectorAll('.square');
+// user prompt
+let userInput;
+
+function howManySquares(){
+    userInput = parseInt(prompt('How many squares do you want on each side of your grid? (Max 100)'));
+    return userInput
+}
+
+// grid reset
+let squares = document.querySelectorAll('.square');
+
+function unsketch(){
+    squares.forEach(item => item.classList.remove('sketched'));
+    squares.forEach(item => item.classList.add('unsketched'));
+}
+// reset grid template
+function resetGridTemplate(){
+    gridcontainer.style.gridTemplateRows = "none";
+    gridcontainer.style.gridTemplateColumns = "none";
+
+}
+// set new grid container
+const gridcontainer = document.querySelector('.gridcontainer');
+
+function setGridRow(){
+    gridcontainer.style.gridTemplateRows = `repeat(${userInput}, auto)`;
+}
+
+function setGridColumn(){
+    gridcontainer.style.gridTemplateColumns = `repeat(${userInput}, auto)`;
+}
+
+// reset the grid and prompt user for new grid
+function newGrid(){
+    unsketch();
+    removeOldSquares();
+    resetGridTemplate();
+    howManySquares();
+    setGridRow();
+    setGridColumn();
+    createGrid();
+    squares = document.querySelectorAll('.square');
+    sketchTheGrid();
+}
+
+// set initial grid
+function initialGrid(){
+    unsketch();
+    resetGridTemplate();
+    howManySquares();
+    setGridRow();
+    setGridColumn();
+    createGrid();
+    squares = document.querySelectorAll('.square');
+    sketchTheGrid();
+}
+
+initialGrid();
+
+
+
+// create squares
+
+function createGrid(){
+
+    let i = 1;
+
+    let column = userInput;
+    let grid = userInput * column;
+
+    while (i <= grid){
+        let square = document.createElement('div');
+        square.style.cssText = "width:100%; height:100%";
+        square.classList.add(`square`, `${i}`, `unsketched`);
+        gridcontainer.appendChild(square);
+        i++;
+    }
+}
+
+
+// sketch the squares
 
 
 function sketch(){
     this.classList.add('sketched');
     this.classList.remove('unsketched');
 }
+function sketchTheGrid(){
 
 squares.forEach(item => {item.addEventListener('mouseover', sketch)  
 });
@@ -27,8 +98,6 @@ squares.forEach(item => {item.addEventListener('mouseover', sketch)
 squares.forEach(item => {item.addEventListener('mousedown', sketch)  
 });
 
-
-function unsketch(){
-    squares.forEach(item => item.classList.remove('sketched'));
-    squares.forEach(item => item.classList.add('unsketched'));
 }
+
+
