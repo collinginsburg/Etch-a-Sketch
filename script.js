b47fc183db6d8de067c6f050d702251e5916e12d
collinginsburg <chginsburg@email.wm.edu>
2021-05-05 17:31:18 -0700

    Decent ending place today. Got the opacity feature to FINALLY work. But now im realizing that my original method of swapping classes from unsketched to sketch doesnt realy make much sense, is preventing me from starting the 'sketch' at the opacity level i would like, and MAYBE is causing the performance issues ive noticed.. tomorrow i will try to figure all that out. I think its as simple as setting the original grid to a color, w/o a class, and then applying event listeners which run the darken function...

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-// user prompt
+// *****************************etch a sketch*********************************
+
+// 1. grid reset
+let squares = document.querySelectorAll('.square');
+
+function unsketch(){
+    squares.forEach(item => item.classList.remove('sketched'));
+    squares.forEach(item => item.classList.add('unsketched'));
+}
+
+// 2. remove old squares
+function removeOldSquares(){
+    squares.forEach(item => gridcontainer.removeChild(item));
+}
+
+
+// 3. reset grid template
+function resetGridTemplate(){
+    gridcontainer.style.gridTemplateRows = "none";
+    gridcontainer.style.gridTemplateColumns = "none";
+}
+
+// 4. user prompt - how many squares
 let userInput;
 
 function howManySquares(){
@@ -6,6 +28,80 @@ function howManySquares(){
     userInput = slider.value; // sliders value goes into user input variable
     return userInput
 }
+
+// 5. & 6. set new grid container
+const gridcontainer = document.querySelector('.gridcontainer');
+
+function setGridRow(){
+    gridcontainer.style.gridTemplateRows = `repeat(${userInput}, auto)`;
+}
+function setGridColumn(){
+    gridcontainer.style.gridTemplateColumns = `repeat(${userInput}, auto)`;
+}
+
+// 7. create grid squares
+function createGrid(){
+
+    let i = 1;
+    let column = userInput;
+    let grid = userInput * column;
+
+    while (i <= grid){
+        let square = document.createElement('div');
+        square.style.cssText = "width:100%; height:100%";
+        square.classList.add(`square`, `${i}`, `unsketched`);
+        gridcontainer.appendChild(square);
+        i++;
+    }
+}
+
+// 8. sketch the squares
+
+let sketched;
+
+function sketch(e){
+    e.target.classList.add('sketched');
+    e.target.classList.remove('unsketched');
+    sketched = document.querySelectorAll('.sketched');
+    sketched.forEach(item => {item.addEventListener('mouseover', addOpacity);  
+    });
+    sketched.forEach(item => {item.addEventListener('touchstart', addOpacity);  
+});
+         
+}
+
+function sketchTheGrid(){
+    squares.forEach(item => {item.addEventListener('mouseover', sketch)  
+    });
+}
+
+// 9. add opacity to squares
+let currentOpacity;
+function addOpacity(e){
+    // console.log(e.target.style.opacity);
+    currentOpacity = Number(e.target.style.opacity);
+    // console.log(currentOpacity);
+    slightlyDarker = currentOpacity + .1;
+
+    if (currentOpacity < 1){
+        e.target.style.opacity = slightlyDarker;
+        console.log(slightlyDarker);
+    }
+}
+
+// 10. allow user to toggle between gradient and non-gradient
+
+// let 
+
+
+
+
+
+
+
+
+
+// *******************************BUTTONS!!!!*******************************
 
 // slider
 
@@ -21,72 +117,18 @@ slider.oninput = function() {
     }
 }
 
-// grid reset
-let squares = document.querySelectorAll('.square');
-
-function unsketch(){
-    squares.forEach(item => item.classList.remove('sketched'));
-    squares.forEach(item => item.classList.add('unsketched'));
-}
-
-// remove old squares
-function removeOldSquares(){
-    squares.forEach(item => gridcontainer.removeChild(item));
-}
-// reset grid template
-function resetGridTemplate(){
-    gridcontainer.style.gridTemplateRows = "none";
-    gridcontainer.style.gridTemplateColumns = "none";
-
-}
-// set new grid container
-const gridcontainer = document.querySelector('.gridcontainer');
-
-function setGridRow(){
-    gridcontainer.style.gridTemplateRows = `repeat(${userInput}, auto)`;
-}
-function setGridColumn(){
-    gridcontainer.style.gridTemplateColumns = `repeat(${userInput}, auto)`;
-}
-
-// create squares
-function createGrid(){
-
-    let i = 1;
-
-    let column = userInput;
-    let grid = userInput * column;
-
-    while (i <= grid){
-        let square = document.createElement('div');
-        square.style.cssText = "width:100%; height:100%";
-        square.classList.add(`square`, `${i}`, `unsketched`);
-        gridcontainer.appendChild(square);
-        i++;
-    }
-}
-
-// sketch the squares
-function sketch(){
-    this.classList.add('sketched');
-    this.classList.remove('unsketched');
-}
-function sketchTheGrid(){
-
-squares.forEach(item => {item.addEventListener('mouseover', sketch)  
-});
-
-squares.forEach(item => {item.addEventListener('touchmove', sketch)  
-});
-
-}
-
 // reset button
 
 const BUTTON = document.querySelector('.resetButton');
 BUTTON.addEventListener('click', newGrid); 
 
+
+
+
+// *******************************BIG OPERATIONAL FUNCTIONS****************************
+
 // reset the grid and input user's slider value
+
 function newGrid(){
     unsketch();
     removeOldSquares();
@@ -100,6 +142,7 @@ function newGrid(){
 }
 
 // set initial grid function
+
 function initialGrid(){
     unsketch();
     resetGridTemplate();
@@ -112,6 +155,6 @@ function initialGrid(){
 }
 
 
+// *******************************on page load*******************************
 
-// on page load
 initialGrid();
